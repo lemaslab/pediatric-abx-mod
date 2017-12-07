75ea63cae71d5c1a0af56442cb890c08d27ec14a
dlemas <dominicklemas@gmail.com>
2017-12-06 23:06:00 -0500

    clinic import complete

--- getData_BabyBillingCodesHospital.R
+++ getData_BabyBillingCodesHospital.R
@@ -35,12 +35,33 @@ library(tidyr)
 library(dplyr)
 
 # **************************************************************************** #
-# ***************                Mom Prenatals.xlsx                                              
+# ***************                Baby-Billing Codes (Hospital).xlsx                                              
 # **************************************************************************** #      
 
 # file parameters
 n_max=10000
 data.file.name="Baby-Billing Codes (Hospital).xlsx";data.file.name
+
+# **************************************************************************** #
+# ***************                 baby_hospital_asthma                                              
+# **************************************************************************** #
+
+baby.asthma=read_xlsx(paste(data.dir,data.file.name,sep=""), sheet = "Asthma", range = NULL, col_names = TRUE,
+                      col_types = NULL, na = "NA", trim_ws = TRUE, skip = 0, n_max = Inf,
+                      guess_max = min(1000, n_max));baby.asthma
+
+# data
+dat=baby.asthma
+
+# rename
+newdata=rename(dat, part_id = `Baby Id`, infant_asthma_date=`Service/Charge Date`, infant_asthma_icd=`Asthma ICD9/ICD10`);newdata
+# unique ID? Some moms had multiple babies in data set
+length(unique(newdata$part_id)) # 16346
+length(newdata$part_id)         # 16353
+names(newdata); head(newdata)
+# sort
+newdata2 <- newdata[order(newdata$part_id, as.Date(newdata$infant_asthma_date,format='%Y-%m-%d')),]
+names(newdata2); head(newdata2)
 
 # **************************************************************************** #
 # ***************                 baby_hospital_dermatitis                                              
@@ -50,7 +71,34 @@ data.file.name="Baby-Billing Codes (Hospital).xlsx";data.file.name
 # ***************                 baby_hospital_ear                                              
 # **************************************************************************** #
 
+
 # **************************************************************************** #
 # ***************                 baby_hospital_eczema                                              
 # **************************************************************************** #
+
+# **************************************************************************** #
+# ***************                 baby_hospital_foodallergy                                              
+# **************************************************************************** #
+
+
+# **************************************************************************** #
+# ***************                 baby_hospital_hemangioma                                              
+# **************************************************************************** #
+
+
+# **************************************************************************** #
+# ***************                 baby_hospital_sebaceous                                              
+# **************************************************************************** #
+
+
+# **************************************************************************** #
+# ***************                 baby_hospital_obesity                                              
+# **************************************************************************** #
+
+
+# **************************************************************************** #
+# ***************                 baby_hospital_erythema                                              
+# **************************************************************************** #
+
+
 
