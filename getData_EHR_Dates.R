7d5e36b540c65dedd6de20cae32ab5ac4b73b961
dlemas <djlemas@ufl.edu>
2018-01-06 21:54:22 -0500

    compute days2measure: phase 1

--- getData_EHR_Dates.R
+++ getData_EHR_Dates.R
@@ -6,23 +6,22 @@
 # Author:      Dominick Lemas 
 # Date:        Sept 09 2017
 # IRB:
-# Description: compute infant "date to occurance" variables for import to RedCap
+# Description: Import dates that were exported from RedCap to compute days2measure. 
+# Data: C:\Users\Dominick\Dropbox (UFL)\IRB\UF\UFHealth\redcap_import\dates
 
 # **************************************************************************** #
 # ***************                Directory Variables           *************** #
 # **************************************************************************** #
-
-rm(list=ls())
-graphics.off()
 
 # Computer
 # location="djlemas";location
 # location="Dominick";location
 
 # Directory Locations
-work.dir=paste("C:\\Users\\",location,"\\Dropbox\\IRB\\UF\\UFHealth\\redcap_import\\01_import_22July17\\EXPORT\\",sep="");work.dir
-data.dir=work.dir
-  
+work.dir=paste("C:\\Users\\",location,"\\Dropbox (UFL)\\IRB\\UF\\UFHealth\\redcap_import\\raw_data\\",sep="");work.dir
+data.dir=paste("C:\\Users\\",location,"\\Dropbox (UFL)\\IRB\\UF\\UFHealth\\redcap_import\\raw_data\\",sep="");data.dir
+
+# Set Working Directory
 setwd(work.dir)
 list.files()
 
@@ -30,98 +29,51 @@ list.files()
 # ***************                Library                       *************** #
 # **************************************************************************** #
 
+library(readxl)
 library(data.table)
 library(tidyr)
-library(tidyverse)
-library(reshape2)
-library(lubridate)
 library(dplyr)
 
 # **************************************************************************** #
-# ***************                # LOAD DATA                                              
+# ***************                EHR_dates.xlsx                                              
 # **************************************************************************** #      
 
-# FIRST DATA SET
-##--------------
-
 # file parameters
-data.file.name="UFHealth_Dates_DATA_30Aug17.csv";data.file.name
-
-# load data 1
-baby.data <- read.csv(paste(data.dir,data.file.name,sep=""))
-head(baby.data)
-str(baby.data);dim(baby.data)
-names(baby.data)
+n_max=10000
+data.file.name="EHR_dates.xlsx";data.file.name
 
 # **************************************************************************** #
-# ***************                # FORMAT DATE VARIABLE(S)                                             
-# **************************************************************************** # 
+# ***************                baby_wellness_dates                                              
+# **************************************************************************** #
 
-# baby_id
-baby.data$baby_id=as.character(baby.data$baby_id)
+# baby_wellness_dates
+#-----------------
+# rows: 
+# cols:  
+# unique id: 
+# repeat: 
+# ICD9/10: 
 
-# modify data
-dat=baby.data
+# read data
+baby.wellness=read_xlsx(paste(data.dir,data.file.name,sep=""), sheet = "baby_wellness_dates", range = NULL, col_names = TRUE,
+                   col_types = NULL, na = "NA", trim_ws = TRUE, skip = 0, n_max = Inf,
+                   guess_max = min(1000, n_max));baby.wellness
 
-head(dat)
-str(dat)
-
-dat[1,1:5]
-str(dat)
-
-# dates
-dat[, 5:29] <-sapply(dat[, 5:29], as.character)
-
-dat$dob=as.Date(dat$dob, "%m/%d/%Y")
-dat$admit_date=as.Date(dat$admit_date, "%m/%d/%Y")
-dat$immune_date=as.Date(dat$immune_date, "%m/%d/%Y")
-dat$observation_date=as.Date(dat$observation_date, "%m/%d/%Y")
-dat$first_height_date=as.Date(dat$first_height_date, "%m/%d/%Y")
-dat$first_head_circumf_date=as.Date(dat$first_head_circumf_date, "%m/%d/%Y")
-dat$med_date=as.Date(dat$med_date, "%m/%d/%Y")
-dat$med_ip_date=as.Date(dat$med_ip_date, "%m/%d/%Y")
-dat$asthma_charge_date=as.Date(dat$asthma_charge_date, "%m/%d/%Y")
-dat$food_allergy_charge_date=as.Date(dat$food_allergy_charge_date, "%m/%d/%Y")
-dat$ear_infect_charge_date=as.Date(dat$ear_infect_charge_date, "%m/%d/%Y")
-dat$eczema_charge_date=as.Date(dat$eczema_charge_date, "%m/%d/%Y")
-dat$dermatitis_charge_date=as.Date(dat$dermatitis_charge_date, "%m/%d/%Y")
-dat$erythema_charge_date=as.Date(dat$erythema_charge_date, "%m/%d/%Y")
-dat$sebaceous_charge_date=as.Date(dat$sebaceous_charge_date, "%m/%d/%Y")
-dat$hemangioma_charge_date=as.Date(dat$hemangioma_charge_date, "%m/%d/%Y")
-dat$asthma_hospital_admit_date=as.Date(dat$asthma_hospital_admit_date, "%m/%d/%Y")
-dat$dermatitis_hosp_admit_date=as.Date(dat$dermatitis_hosp_admit_date, "%m/%d/%Y")
-dat$ear_hospital_admit_date=as.Date(dat$ear_hospital_admit_date, "%m/%d/%Y")
-dat$asthma_hospital_admit_date=as.Date(dat$asthma_hospital_admit_date, "%m/%d/%Y")
-dat$dermatitis_hosp_admit_date=as.Date(dat$dermatitis_hosp_admit_date, "%m/%d/%Y")
-dat$ear_hospital_admit_date=as.Date(dat$ear_hospital_admit_date, "%m/%d/%Y")
-dat$eczema_hospital_admit_date=as.Date(dat$eczema_hospital_admit_date, "%m/%d/%Y")
-dat$food_allergy_hosp_admit_date=as.Date(dat$food_allergy_hosp_admit_date, "%m/%d/%Y")
-dat$hemangioma_hosp_admit_date=as.Date(dat$hemangioma_hosp_admit_date, "%m/%d/%Y")
-dat$sebaceous_hosp_admit_date=as.Date(dat$sebaceous_hosp_admit_date, "%m/%d/%Y")
-dat$obesity_hospital_admit_date=as.Date(dat$obesity_hospital_admit_date, "%m/%d/%Y")
-dat$toxicum_hosp_admit_date=as.Date(dat$toxicum_hosp_admit_date, "%m/%d/%Y")
-
-# check
-head(dat);names(dat)
-
-# test
-test1=dat[,c(1,3:29)];names(test1);head(test1)
-
-# *******************************************c*********************************#
-# ***************                # FORMAT DATE OF BIRTH                                            
-# **************************************************************************** # 
+# data
+dat=baby.wellness
+head(dat); str(dat)
 
 # melt data
-mdata <- melt(test1, id=c("baby_id","redcap_repeat_instrument","redcap_repeat_instance","dob"))
+mdata <- melt(dat, id=c("part_id","redcap_repeat_instrument","redcap_repeat_instance","redcap_event_name","infant_dob"))
 head(mdata)[3];str(mdata);dim(mdata)
-length(unique(mdata$baby_id))
+length(unique(mdata$part_id))
 length(is.na(mdata$value)==F)
 mdata[1:50,]
 
 # remove 
 mdata.d=subset(mdata, is.na(value)==F);dim(mdata.d)
 head(mdata.d);
-length(unique(mdata.d$baby_id))
+length(unique(mdata.d$part_id))
 mdata.d[,"dob.new"]=NA
 # mdata.d$dob=as.numeric(mdata.d$dob)
 str(mdata.d)
@@ -130,14 +82,14 @@ mdata.d1=mdata.d
 head(mdata.d1);
 dim(mdata.d1)
 mdata.d1$redcap_repeat_instrument2=mdata.d1$redcap_repeat_instrument
-mdata.d1$dob2=mdata.d1$dob
+mdata.d1$dob2=mdata.d1$infant_dob
 
 # drop duplicate: Baby-0004
 mdata.d2=mdata.d1[-122281,]
 
 dat.new=mdata.d2 %>%
-  group_by(baby_id) %>%
-  spread(redcap_repeat_instrument, dob) %>%  # makes this variable into its own columns (long to wide)
+  group_by(part_id) %>%
+  spread(redcap_repeat_instrument, infant_dob) %>%  # makes this variable into its own columns (long to wide)
   select(baby_id, redcap_repeat_instrument2,redcap_repeat_instance, variable, value,dob2, babybaby) %>%
   mutate(birth = first(babybaby))  # Repeat the first observation for birth within each baby
 
