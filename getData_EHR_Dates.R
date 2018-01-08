5d2dd1257d92eae49d6932a7252cde3f890db09f
dlemas <dominicklemas@gmail.com>
2018-01-08 16:39:42 -0500

    days2measure: wellness

--- getData_EHR_Dates.R
+++ getData_EHR_Dates.R
@@ -30,9 +30,12 @@ list.files()
 # **************************************************************************** #
 
 library(readxl)
+library(tidyverse)
 library(data.table)
 library(tidyr)
 library(dplyr)
+library(reshape2)
+library(lubridate)
 
 # **************************************************************************** #
 # ***************                EHR_dates.xlsx                                              
@@ -54,79 +57,72 @@ data.file.name="EHR_dates.xlsx";data.file.name
 # repeat: 
 # ICD9/10: 
 
+# clear slate
+# rm(baby.wellness,test, dat, dat.new,new,data,newdata,mdata, mdata.d, mdata.d1, mdata.d2, newdata2)
+
 # read data
 baby.wellness=read_xlsx(paste(data.dir,data.file.name,sep=""), sheet = "baby_wellness_dates", range = NULL, col_names = TRUE,
                    col_types = NULL, na = "NA", trim_ws = TRUE, skip = 0, n_max = Inf,
                    guess_max = min(1000, n_max));baby.wellness
 
 # data
-dat=baby.wellness
-head(dat); str(dat)
+newdata=baby.wellness
+names(newdata);head(newdata)
 
 # melt data
-mdata <- melt(dat, id=c("part_id","redcap_repeat_instrument","redcap_repeat_instance","redcap_event_name","infant_dob"))
-head(mdata)[3];str(mdata);dim(mdata)
-length(unique(mdata$part_id))
-length(is.na(mdata$value)==F)
+mdata <- melt(newdata, id=c("part_id","redcap_event_name","redcap_repeat_instrument","redcap_repeat_instance","infant_dob"))
+head(mdata);str(mdata);dim(mdata)
+length(unique(mdata$part_id))  # 30540
+length(is.na(mdata$value)==F)  # 2071363
 mdata[1:50,]
+mdata$redcap_repeat_instrument2=mdata$redcap_repeat_instrument
 
-# remove 
-mdata.d=subset(mdata, is.na(value)==F);dim(mdata.d)
+# remove non-sense rows (no data) 
+mdata.d=subset(mdata, is.na(value)==F);dim(mdata.d) # 282053
 head(mdata.d);
 length(unique(mdata.d$part_id))
-mdata.d[,"dob.new"]=NA
-# mdata.d$dob=as.numeric(mdata.d$dob)
-str(mdata.d)
+str(mdata.d); head(mdata.d)
+newdata2 <- mdata.d[order(mdata.d$part_id, mdata.d$redcap_repeat_instrument),]
+head(newdata2)
+newdata2[1:50,]
 
-mdata.d1=mdata.d
-head(mdata.d1);
-dim(mdata.d1)
-mdata.d1$redcap_repeat_instrument2=mdata.d1$redcap_repeat_instrument
-mdata.d1$dob2=mdata.d1$infant_dob
-
-# drop duplicate: Baby-0004
-mdata.d2=mdata.d1[-122281,]
-
-dat.new=mdata.d2 %>%
+# dplyr for data wrangle
+#----------------------
+dat.new=newdata2 %>%
   group_by(part_id) %>%
   spread(redcap_repeat_instrument, infant_dob) %>%  # makes this variable into its own columns (long to wide)
-  select(baby_id, redcap_repeat_instrument2,redcap_repeat_instance, variable, value,dob2, babybaby) %>%
-  mutate(birth = first(babybaby))  # Repeat the first observation for birth within each baby
+  select(part_id, redcap_event_name, redcap_repeat_instance,redcap_repeat_instrument2, variable, value, baby_demography) %>%
+  mutate(infant_dob = first(baby_demography[!is.na(baby_demography)])) %>%  # Repeat the first observation for birth within each baby
+  select(part_id,redcap_repeat_instrument2, redcap_event_name, redcap_repeat_instance, infant_dob, variable, value) %>% # drop and reorder variables
+  mutate(date=as.Date(value, format="%Y-%m-%d"))
 
-# check
+# check (it works!)
 head(dat.new)
-dat.new[1,]
+dat.new[1:50,]
+str(dat.new)
 
-# **************************************************************************** #
-# ***************                # days_to variable(s)                                             
-# **************************************************************************** #      
-
-dat.new$days_to_meas=dat.new$value-dat.new$birth
+# days_to variable(s)
+dat.new$days_to_meas=dat.new$date-as.Date(dat.new$infant_dob,format="%Y-%m-%d")
 head(dat.new)
 range(dat.new$days_to_meas)
-
-# **************************************************************************** #
-# ***************                # MODIFY FOR REDCAP IMPORT                                              
-# **************************************************************************** #      
-
-# drop
 names(dat.new)
-dat.new2=dat.new[,c(1:4,9)]
-head(dat.new2)
+
+# modify redcap import
+dat.new.sort=dat.new %>% 
+  as_data_frame %>%
+  select_("part_id","redcap_repeat_instrument2","redcap_repeat_instance","redcap_event_name","variable","days_to_meas")
+names(dat.new.sort);head(dat.new.sort)
+is.data.table(dat.new.sort)
 
 # cast
-dat.new3=dcast(dat.new2, baby_id+redcap_repeat_instrument2+redcap_repeat_instance~variable)
-head(dat.new3)
-
-# add
-dat.new3$redcap_event_name=paste("visit_",dat.new3$redcap_repeat_instance,"_arm_1",sep="")
+dat.new3=dcast(dat.new.sort, part_id+redcap_repeat_instrument2+redcap_event_name+redcap_repeat_instance~variable)
 head(dat.new3)
 names(dat.new3)
 
-# modify names
+# modify names (start here)
 dat.new4=rename(dat.new3, redcap_repeat_instrument=redcap_repeat_instrument2,
                           days2_admit=admit_date,
-                          days2_vaccine=immune_date,
+                          days2_inf_vaccine=infant_immune_date,
                           days2_wellvisit=observation_date,
                           days2_ht1=first_height_date,
                           days2_hc1=first_head_circumf_date,
