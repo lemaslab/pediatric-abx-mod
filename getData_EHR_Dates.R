c30cd879ad7bf750fa2390226e98fa683c28b450
dlemas <dominicklemas@gmail.com>
2018-01-09 15:00:38 -0500

    EHR wellness: days2measure computed

--- getData_EHR_Dates.R
+++ getData_EHR_Dates.R
@@ -4,7 +4,7 @@
 # **************************************************************************** #
            
 # Author:      Dominick Lemas 
-# Date:        Sept 09 2017
+# Date:        January 09 2018
 # IRB:
 # Description: Import dates that were exported from RedCap to compute days2measure. 
 # Data: C:\Users\Dominick\Dropbox (UFL)\IRB\UF\UFHealth\redcap_import\dates
@@ -121,41 +121,31 @@ names(dat.new3)
 
 # modify names (start here)
 dat.new4=rename(dat.new3, redcap_repeat_instrument=redcap_repeat_instrument2,
-                          days2_admit=admit_date,
-                          days2_inf_vaccine=infant_immune_date,
-                          days2_wellvisit=observation_date,
-                          days2_ht1=first_height_date,
-                          days2_hc1=first_head_circumf_date,
-                          days2_meds=med_date,
-                          days2_meds_ip=med_ip_date,
-                          days2_asthma_clinic=asthma_charge_date,
-                          days2_fa_clinic=food_allergy_charge_date,
-                          days2_ear_clinic=ear_infect_charge_date,
-                          days2_eczema_clinic=eczema_charge_date,
-                          days2_dermatitis_clinic=dermatitis_charge_date,
-                          days2_erythema_clinic=erythema_charge_date,
-                          days2_sebaceous_clinic=sebaceous_charge_date,
-                          days2_hemangioma_clinic=hemangioma_charge_date,
-                          days2_asthma_hosp=asthma_hospital_admit_date,
-                          days2_dermatitis_hosp=dermatitis_hosp_admit_date,
-                          days2_ear_hosp=ear_hospital_admit_date,
-                          days2_eczema_hosp=eczema_hospital_admit_date,
-                          days2_fa_hosp=food_allergy_hosp_admit_date,
-                          days2_hemangioma_hosp=hemangioma_hosp_admit_date,
-                          days2_sebaceous_hosp=sebaceous_hosp_admit_date,
-                          days2_obesity_hosp=obesity_hospital_admit_date,
-                          days2_erythema_hosp=toxicum_hosp_admit_date);names(dat.new4)
+                days2_baby_admit=infant_admit_date,
+                days2_baby_vac=infant_immune_date,
+                days2_baby_wellvisit=infant_obs_date,
+                days2_baby_ht1=infant_ht1_date,
+                days2_baby_hc1=infant_hc1_date,
+                days2_baby_meds=infant_med_date,
+                days2_baby_meds_ip=infant_med_ip_date);names(dat.new4)
 head(dat.new4)
-dat.new5=dat.new4[,c(1:3,28,4:27)];names(dat.new5)
-head(dat.new5)
+dt5=dat.new4
 
-# **************************************************************************** #
-# ***************                # EXPORT DATA                                              
-# **************************************************************************** #
-# file parameters
-data.file.name.export="baby.dates_08Sept17.csv";data.file.name.export
-head(dat.new5)
+# export data
+#-------------
+batchSize=10000; # number of rows in single output file
+data.file.name.export="baby_wellness_dates";data.file.name.export
+out.dir=paste("C:\\Users\\",location,"\\Dropbox (UFL)\\IRB\\UF\\UFHealth\\redcap_import\\03_redcap_import_Jan18\\",sep="");out.dir
 
-# write file
-write.table(dat.new5, file =(paste(data.dir,data.file.name.export,sep="")),row.names=F, sep=";")
+chunks=split(dt5, floor(0:(nrow(dt5)-1)/batchSize))
+for (i in 1:length(chunks))
+{ # second loop
+  write.table(chunks[[i]],paste0(out.dir,data.file.name.export,i,'.csv'),row.names=F, sep="\t")
+} # end second loop
+
+# clear slate
+rm(baby.wellness,chunks,dat.new,dat.new.sort,dat.new3,dat.new4,dt5,mdata,mdata.d,newdata,newdata2)
+
+
+
 
