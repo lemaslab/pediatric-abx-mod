92a3ea7aab2d200810487547258cdd942cc66afa
dlemas <djlemas@ufl.edu>
2018-03-05 21:30:09 -0500

    infant episode counter

--- dataWrangle_infant_ABX.R
+++ dataWrangle_infant_ABX.R
@@ -63,41 +63,7 @@ head(dat.s)
 dat.s1=subset(dat.s, is.na(days2_baby_meds_ip)==F)
 head(dat.s1)
 
-# https://stackoverflow.com/questions/36187931/dplyr-grouping-and-summarizing-mutating-data-with-rolling-time-windows
-
-
-# https://stackoverflow.com/questions/14454476/get-the-difference-between-dates-in-terms-of-weeks-months-quarters-and-years
-
-# Convert to Datetime Objects
-date_strings = dat.s1$baby_med_ip_date;date_strings[1:2]
-datetimes = strptime(date_strings, format = "%m/%d/%Y");datetimes[1:2] # convert to datetime objects
-
-# Difference in Days
-# You can use the diff in days to get some of our later answers
-diff_in_days = difftime(datetimes[2], datetimes[1], units = "days") # days
-diff_in_days
-#Time difference of 435.9583 days
-
-
-# new variable
-dat.s1$new=NA
-
-
-head(dat.s1)
-
-dat.s1$new=ifelse()
-
-# need to first count antibiotics per day
-
-dat.s1 %>% 
-  group_by(part_id) %>%
-  tally(baby_med_ip_date)
-  summarise(count_days = sum(sleep_total), 
-            min_sleep = min(sleep_total), 
-            max_sleep = max(sleep_total),
-            total = n())
-
-
+# mock data
 df2=data.frame(
   id = c(01,01,01,01,01,02,02,03,04),
   date = c("2015-01-01 11:00", 
@@ -111,19 +77,27 @@ df2=data.frame(
            "2015-08-01 13:15"),
   abx = c("AMPICILLIN","ERYTHROMYCIN","NEOMYCIN","AMPICILLIN","VANCOMYCIN","VANCOMYCIN","NEOMYCIN","PENICILLIN","ERYTHROMYCIN"));df2
 
+# function
+grpno <- function(x) cumsum(c(TRUE, diff(x) >=7 ))
+transform(df2, episode = ave(as.numeric(as.Date(date)), id, FUN = grpno))
 
-id             date          abx    episode
-1  1 2015-01-01 11:00   AMPICILLIN  1 
-2  1 2015-01-06 13:29 ERYTHROMYCIN  1
-3  1 2015-01-10 12:46     NEOMYCIN  1
-4  1 2015-01-25 14:45   AMPICILLIN  2
-5  1 2015-02-15 13:30   VANCOMYCIN  3
-6  2 2015-01-01 10:00   VANCOMYCIN  
-7  2 2015-05-05 15:20     NEOMYCIN
-8  3 2015-01-01 15:19   PENICILLIN
-9  4 2015-08-01 13:15 ERYTHROMYCIN
+# or with dplyr
+df2 %>%
+  group_by(id) %>%
+  mutate(episode = date %>% as.Date %>% as.numeric %>% grpno) %>%
+  ungroup
 
+# real data
+head(dat.s1)
+test=dat.s1 %>%
+  group_by(part_id) %>%
+  mutate(date = as.Date(baby_med_ip_date, format="%m/%d/%Y")) %>% 
+  mutate(episode=which(first(date)==1)
+         
+test2=test %>%
+  group_by(part_id) %>%
+  mutate(test=first(date)==T)
+head(test)
+test2[,c(1,3:5)]
+test2$test
 
-df_full<- df2 %>%
-  mutate(date=as.Date(date))  %>%
-  complete(id, date=seq(from=min(.$date)-30,to=max(.$date), by=1), fill=list(n_widgets=0))
