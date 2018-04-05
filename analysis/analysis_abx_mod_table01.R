120e42d38cfc1f87a95180bcfe6fab6a3dc6dd18
dlemas <dominicklemas@gmail.com>
2018-04-05 11:03:23 -0400

    abx table 1

--- analysis/analysis_abx_mod_table01.R
+++ analysis/analysis_abx_mod_table01.R
@@ -40,9 +40,12 @@ library(reshape2)
 # *****      l             load data: abx_mod_04Apr18.rdata       
 # **************************************************************************** # 
 
-load(file="abx_mod_04Apr18.rdata")
+load(file="abx_mod_05Apr18.rdata")
 head(dat2)
 names(dat2)
+
+# quick diagnostics
+length(unique(dat2$part_id)) # 13661
 
 # **************************************************************************** #
 # *****      subset the data: gest_age (wk) >37 & <42, birth_wt (gr) >500                                               
@@ -54,10 +57,41 @@ dat3=dat2 %>%
 # check
 range(dat3$gest_age_wk)
 range(dat3$baby_birth_wt_gr)
+length(unique(dat3$part_id)) # 5194
 
 # **************************************************************************** #
 # *****      formatt/subset the data: single observations                                              
 # **************************************************************************** # 
 
-#
+# create table
 names(dat3)
+range(dat3$redcap_repeat_instance)
+
+dat4=dat3 %>%
+  filter(redcap_repeat_instance==1)
+# check
+range(dat4$gest_age_wk)
+range(dat4$baby_birth_wt_gr)
+length(unique(dat4$part_id)) # 4942
+
+head(dat4)
+dat4$abx_episode_max
+
+# https://stackoverflow.com/questions/34587317/using-dplyr-to-create-summary-proportion-table-with-several-categorical-factor-v
+
+dat4 %>%
+  summarize(dat4)
+m_mtcars <- melt(mtcars,measure.vars=c("gear","carb","cyl"))
+res <- m_mtcars %>%
+  group_by(am, variable, value) %>%
+  summarise (n = n()) %>%
+  mutate(freq = n / sum(n))
+
+
+mtcars %>%
+  gather(variable, value, gear, carb, cyl) %>% # head()
+  group_by(am, variable, value) %>%
+  summarise (n = n()) %>%
+  mutate(freq = n / sum(n))
+  
+  
