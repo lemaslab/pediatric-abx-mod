97406205f4b6c87b55565cc64b838269fb733d27
dlemas <djlemas@ufl.edu>
2019-03-22 20:23:09 -0400

    API token for EHR

---
+++ getData/getData_RedCapR.R
@@ -0,0 +1,33 @@
+##-------------- 
+# **************************************************************************** #
+# ***************                Project Overview              *************** #
+# **************************************************************************** #
+
+# Author:      Dominick Lemas 
+# Date:        March 22, 2019 
+# IRB:
+# Description: RedCap API system for UFHealth EHR project
+# Data: C:\Users\djlemas\Dropbox (UFL)\02_Projects\UFHEALTH\figures
+
+# **************************************************************************** #
+# ***************                Library                       *************** #
+# **************************************************************************** #
+
+library(keyringr)
+library(REDCapR)
+library(httr)
+library(redcapAPI)
+
+# Get Redcap API Token
+# # https://cran.r-project.org/web/packages/keyringr/vignettes/Avoiding_plain_text_passwords_in_R_with_keyringr.html
+credential_label <- "EHR_API"
+credential_path <- paste(Sys.getenv("USERPROFILE"), '\\DPAPI\\passwords\\', Sys.info()["nodename"], '\\', credential_label, '.txt', sep="")
+uri <- "https://redcap.ctsi.ufl.edu/redcap/api/"
+token <-decrypt_dpapi_pw(credential_path)
+print(token)
+
+# Create connections
+rcon <- redcapConnection(url=uri, token=token)
+
+# list of instruments
+exportInstruments(rcon)
