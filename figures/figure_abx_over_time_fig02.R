98fef79f19f5bd278c9beb281f8a58afc7e05472
Dominick J. Lemas <dominicklemas@gmail.com>
2019-06-27 13:14:41 -0400

    update fig2

--- figures/figure_abx_over_time_fig02.R
+++ figures/figure_abx_over_time_fig02.R
@@ -8,6 +8,7 @@
 # IRB:
 # Description: Cummulative mean function of Abx over time paper #1
 # Data: C:\Users\djlemas\Dropbox (UFL)\02_Projects\UFHEALTH\figures
+# UPDATE: Modifying sample size.
 
 # **************************************************************************** #
 # ***************                Directory Variables           *************** #
@@ -34,23 +35,26 @@ library(dplyr)
 library(ggplot2)
 
 # **************************************************************************** #
-# *****      load data: Recurrent_Events_Data_for_fig02.csv      
+# *****      load data: fig2_V2_RecurrentEventsDataPlot_27Jun19.csv      
 # **************************************************************************** # 
 
 # read data
-data.file.name="Recurrent_Events_Data_for_fig02.csv";data.file.name
-data.file.path=paste0(data.dir,"\\",data.file.name);data.file.path
+data.file.name="fig2_V2_RecurrentEventsDataPlot_27Jun19.csv";data.file.name
+data.file.path=paste0(data.dir,data.file.name);data.file.path
 time<- read.csv(data.file.path);time
 
 # diagnostics
 dat=time
 dim(dat)
 names(dat)
+str(dat)
+
+# format data
+dat$csection=as.factor(dat$csection)
 
 # plot with days on x-axis
 #-------------------------
 ggplot(dat, aes(x=upper_abx, y=CMF, col=csection)) + geom_line()+
-# p+geom_ribbon(aes(ymin=dat$LowerCMF, ymax=dat$UpperCMF), linetype=0.5, alpha=0.001)
   theme_minimal()+
   xlab("Postnatal Age in Day") + ylab("Mean Antibiotic Episodes")+
   scale_color_discrete(name="Mode of Delivery",labels=c("Vaginal Delivery", "Cesarean Section"))
@@ -65,5 +69,6 @@ ggplot(dat, aes(x=months, y=CMF, col=csection)) + geom_line()+
   theme_minimal()+
   xlab("Postnatal Age in Years") + ylab("Mean Antibiotic Episodes")+
   scale_color_discrete(name="Mode of Delivery",labels=c("Vaginal Delivery", "Cesarean Section"))
+ggsave("fig02_abx_time_V2.png")
 
 
