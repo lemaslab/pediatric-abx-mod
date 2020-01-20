05b5fde1b44bd963f130cd71f1c8df21b72d65f4
Dominick J. Lemas <dominicklemas@gmail.com>
2020-01-20 16:03:37 -0500

    narrow/broad complete

--- figures/figure_abx_over_time_figX.R
+++ figures/figure_abx_over_time_figX.R
@@ -6,7 +6,7 @@
 # Author:      Dominick Lemas 
 # Date:        Jan 14, 2020
 # IRB:
-# Description: narrow/broad abx over time paper #1
+# Description: narrow/broad abx estimates paper #1
 # Data: C:\Users\djlemas\Dropbox (UFL)\02_Projects\UFHEALTH\figures
 # UPDATE: Modifying sample size.
 
@@ -35,6 +35,8 @@ library(ggplot2)
 # *****      load data: AntibioticPrescriptionsbyYear_16Apr2019.xlsx      
 # **************************************************************************** # 
 
+# Frequency Missing = 3817
+
 # read data
 data.file.name="AntibioticPrescriptionsbyYear_16Apr2019.xlsx";data.file.name
 
@@ -47,7 +49,59 @@ abx=read_xlsx(paste(data.dir,data.file.name,sep=""), sheet = "RawDataAntibioticP
                    guess_max = min(1000, n_max));abx
 
 abx$year=as.factor(abx$year)
+table(abx$abx_episode)
+length(unique(abx$Baby_Id)) # 970
 
-abx %>%
-  group_by(year, classification) %>%
-  count()
+# REPLICATE SUPPLEMENTARY TABLE 2
+
+# select highest abx_episode value WITHIN study ID
+new=abx %>%
+  group_by(Baby_Id) %>%
+  mutate(abx_max=last(abx_episode)) %>%
+  distinct(Baby_Id, .keep_all= TRUE)
+
+  # check
+  length(unique(new$Baby_Id)) # 970
+  table(new$abx_max)          # DONE!
+  
+# COMPLETE NARROW ANALYSIS
+
+narrow=abx %>%
+  select(Baby_Id, year, Medication, classification, abx_time) %>%
+  filter(classification=="Narrow") %>%
+  group_by(Baby_Id) %>%
+  arrange(Baby_Id, abx_time) %>%
+  mutate(ticker = row_number(abx_time))
+ length(unique(narrow$Baby_Id))
+
+# select highest abx_episode value WITHIN study ID
+new.narrow=narrow %>%
+  group_by(Baby_Id) %>%
+  mutate(abx_narrow_max=last(ticker)) %>%
+  distinct(Baby_Id, .keep_all= TRUE)
+
+  # check 
+  length(unique(new.narrow$Baby_Id))
+  table(new.narrow$abx_narrow_max)          # DONE!
+  
+# COMPLETE NARROW ANALYSIS
+  
+  broad=abx %>%
+    select(Baby_Id, year, Medication, classification, abx_time) %>%
+    filter(classification=="Broad") %>%
+    group_by(Baby_Id) %>%
+    arrange(Baby_Id, abx_time) %>%
+    mutate(ticker = row_number(abx_time))
+  length(unique(broad$Baby_Id))
+  
+  # select highest abx_episode value WITHIN study ID
+  new.broad=broad %>%
+    group_by(Baby_Id) %>%
+    mutate(abx_broad_max=last(ticker)) %>%
+    distinct(Baby_Id, .keep_all= TRUE)
+  
+  # check 
+  length(unique(new.broad$Baby_Id))
+  table(new.broad$abx_broad_max)          # DONE!
+  
+
